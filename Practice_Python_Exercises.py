8710f6c84acfa18be85ae83529d9c2818e563093
tjobhere <43549603+tjobhere@users.noreply.github.com>
2020-02-03 13:35:07 +0530

    Update Practice_Python_Exercises.py
    Completed Exercise 2

--- Practice_Python_Exercises.py
+++ Practice_Python_Exercises.py
@@ -19,5 +19,18 @@ print('Current Year : ',now.year)
 years_till_100=100-age
 print('Years to go till 100 : ',years_till_100)
 print('Hi {}. You will become 100 years in {}'.format(name,now.year+years_till_100))
-'''
+
 #Exercise 2
+num=int(input('Enter number:'))
+if (num%4)==0:
+    print('Number {} is Even and a multiple of 4'.format(num))
+elif (num%2)==0:
+    print('Number {} is Even and not a multiple of 4'.format(num))
+else:
+    print('Number {} is Odd'.format(num))
+'''
+
+#Exercise 3
+
+print()
+    
