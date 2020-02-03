3cc9899a4099f1a49fdea2cdae31e79abb0f5d0f
tjobhere <43549603+tjobhere@users.noreply.github.com>
2020-02-03 12:22:23 +0530

    Create Practice_Python_Exercises.py

---
+++ Practice_Python_Exercises.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  3 11:10:04 2020
+
+@author: thomas
+"""
+'''
+This is file is to code against all the Exercises given in http://www.practicepython.org/
+
+#Exercise 1
+
+import datetime
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+now=datetime.datetime.now()
+print('Current Year : ',now.year)
+years_till_100=100-age
+print('Years to go till 100 : ',years_till_100)
+print('Hi {}. You will become 100 years in {}'.format(name,now.year+years_till_100))
+'''
+#Exercise 2
