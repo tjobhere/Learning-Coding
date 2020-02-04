34c23f6498b3d7f765d12862a37404d44a53ea98
tjobhere <43549603+tjobhere@users.noreply.github.com>
2020-02-04 19:37:41 +0530

    Update Practice_Python_Exercises.py
    Exercise 10

--- Practice_Python_Exercises.py
+++ Practice_Python_Exercises.py
@@ -127,6 +127,16 @@ while flag=='y' or flag=='Y':
     else:
         print('You guessed lower.')
     flag=input('Continue(y/n)? ')
+
+
+#Exercise 10
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+c = [x for x in a for y in b if x==y]
+d = [x for x in c if c.count(x)==1]
+print(d)
 '''
 
-#
+#Exercise 11
